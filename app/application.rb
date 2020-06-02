9de3cdfcccc03295ac6e95898c1e34ad44c161a8
Jack Madden <jck13mad@gmail.com>
2020-06-02 21:19:22 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -6,7 +6,8 @@ class Application
     
     if req.path.match(/items/)
       item_name = req.path.split("/items/").last 
-      item = @@items.find { |s| s.name 
+      item = @@items.find { |s| s.name == item.name }
+      
   
   
   
